664fd1e704d248e55c2aea54212f9a0c8c6741ac
Gianluca Grande <gianlucagrande10@gmail.com>
2023-10-25 22:51:49 -0300

    lala

--- inicio/models.py
+++ inicio/models.py
@@ -4,4 +4,6 @@ class Curso(models.Model):
     titulo = models.CharField(max_length=50)
     numero = models.IntegerField()
     
+    def __str__(self):
+        return f'{self.titulo} {self.numero}'   
     
